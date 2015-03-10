9cb4719408ac3cfd943403f622290b5cc3337728
Victor Kryukov <victor.kryukov@gmail.com>
2015-03-09 21:03:00 -0700

    Initial commit

---
+++ pento_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPoint(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(Point{1, 0}.Rotate(), Point{0, -1})
+	assert.Equal(Point{0, 1}.Rotate(), Point{1, 0})
+	assert.Equal(Point{1, 1}.Mirror(), Point{1, 1})
+	assert.Equal(Point{1, 0}.Mirror(), Point{0, 1})
+	assert.Equal(Point{12, -5}.Mirror().Mirror(), Point{12, -5})
+
+	assert.True(Point{12, -5}.Mirror().Mirror().Equal(Point{12, -5}))
+}
+
+func TestFigure(t *testing.T) {
+	assert := assert.New(t)
+	f := Figure{Point{1, 0}, Point{0, 1}}
+	assert.True(f.Mirror().Equal(f))
+	f1 := Figure{Point{1, 0}}
+	assert.False(f1.Equal(f))
+	assert.False(f.Equal(f1))
+	assert.False(f1.Mirror().Equal(f1))
+	assert.True(f1.Mirror().Mirror().Equal(f1))
+
+	assert.False(f.Rotate().Equal(f))
+	assert.False(f.Rotate().Rotate().Equal(f))
+	assert.False(f.Rotate().Rotate().Rotate().Equal(f))
+	assert.True(f.Rotate().Rotate().Rotate().Rotate().Equal(f))
+}
