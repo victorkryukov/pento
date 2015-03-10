a1b019f3c4e81a6a57cef434ec30a8e73cf9c90c
Victor Kryukov <victor.kryukov@gmail.com>
2015-03-10 13:15:08 -0700

    First working implementation

--- board.go
+++ board.go
@@ -98,3 +98,35 @@ func (b *Board) String() string {
 	}
 	return buf.String()
 }
+
+var fillIterations int // Cumulative number of Fill calls
+const printAfterNIterations = -1
+
+// Fill tries to place each figure on the board and on success calls it recursively.
+// When board is full, a handler is called.
+func (b *Board) Fill(figures []Figure, handler func(*Board)) {
+	if b.Full() {
+		handler(b)
+		return
+	}
+	for x := 0; x < b.SizeX; x++ {
+		for y := 0; y < b.SizeY; y++ {
+			if !b.Field[x][y] {
+				//fmt.Printf("Trying to place a figure at (%d,%d) in\n%s", x+1, y+1, b)
+				for _, f := range figures {
+					if b.PlaceAt(f, Point{x, y}) {
+						fillIterations++
+						if printAfterNIterations > 0 && fillIterations%printAfterNIterations == 0 {
+							fmt.Println(b)
+						}
+						b.Fill(figures, handler)
+						b.Unplace()
+					} else {
+						//		fmt.Printf("doesn't fit:\n%s\n", f)
+					}
+				}
+				return // Since we are trying to place pentos in the first available place
+			}
+		}
+	}
+}
--- pento.go
+++ pento.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-const Num = 8          // Number of different pento figures, with all mirros and rotations
-var Pentos [Num]Figure // All different pento figures
+var Pentos []Figure // All different pento figures
 
 func init() {
 	p := Figure{Point{0, 0}, Point{0, 1}, Point{1, 1}, Point{0, 2}, Point{0, 3}}
+	Pentos = make([]Figure, 8)
 	Pentos[0] = p
 	Pentos[1] = p.Rotate()
 	Pentos[2] = p.Rotate().Rotate()
@@ -14,10 +19,23 @@ func init() {
 	for i := 0; i <= 3; i++ {
 		Pentos[i+4] = Pentos[i].Mirror()
 	}
+	for i := range Pentos {
+		Pentos[i] = Pentos[i].Recenter()
+	}
+}
+
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("Cannot convert '%s' to int\n", s)
+	}
+	return i
 }
 
 func main() {
-	for _, f := range Pentos {
-		fmt.Println(f)
+	b := NewBoard(atoi(os.Args[1]), atoi(os.Args[2]))
+	printBoard := func(b *Board) {
+		fmt.Println(b)
 	}
+	b.Fill(Pentos, printBoard)
 }
